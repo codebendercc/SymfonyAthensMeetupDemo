7ddf872a508cdd9b325fc661b38f52af0de5f191
Vasilis Georgitzikis <tzikis@gmail.com>
2015-06-29 15:32:19 +0000

    Added functional testing for book addition

--- SymfonyMeetupDemo/src/Codebender/DefaultBundle/Tests/Controller/BookControllerTest.php
+++ SymfonyMeetupDemo/src/Codebender/DefaultBundle/Tests/Controller/BookControllerTest.php
@@ -1,11 +1,11 @@
 <?php
 
-//namespace Codebender\DefaultBundle\Tests\Controller;
+namespace Codebender\DefaultBundle\Tests\Controller;
 
-//use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-//class BookControllerTest extends WebTestCase
-//{
+class BookControllerTest extends WebTestCase
+{
     /*
     public function testCompleteScenario()
     {
@@ -52,4 +52,29 @@
     }
 
     */
-//}
+    public function testAddBook() {
+        // Create a new client to browse the application
+        $client = static::createClient();
+
+        // Navigate to the book addition page
+        $crawler = $client->request('GET', '/book/new');
+        
+        // Fill in the form and submit it
+        $form = $crawler->selectButton('Create')->form(array(
+            'codebender_defaultbundle_book[isbn]'  => '1234',
+            'codebender_defaultbundle_book[title]'  => 'Book title',
+            'codebender_defaultbundle_book[author]'  => 'An author',
+            'codebender_defaultbundle_book[year]'  => '2015',
+            'codebender_defaultbundle_book[shelf]'  => '1',
+            'codebender_defaultbundle_book[section]'  => '2',
+            'codebender_defaultbundle_book[available]'  => true
+        ));
+
+        $client->submit($form);
+        
+        // Navigate to the book addition page
+        $crawler = $client->request('GET', '/book/');
+        
+        $this->assertEquals(1, $crawler->filter('td:Contains("Book title")')->count());
+    }
+}
