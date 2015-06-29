a9ac4d1ca3a4e80f3ae167a5c871e896e4b4213e
Vasilis Georgitzikis <tzikis@gmail.com>
2015-06-29 14:55:13 +0000

    Added another test that covers the rest of the indexAction fnction

--- SymfonyMeetupDemo/src/Codebender/DefaultBundle/Tests/Controller/BookControllerUnitTest.php
+++ SymfonyMeetupDemo/src/Codebender/DefaultBundle/Tests/Controller/BookControllerUnitTest.php
@@ -4,11 +4,7 @@ namespace Codebender\DefaultBundle\Tests\Controller;
 
 class BookControllerUnitTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException \Exception
-     */
-    public function testIndexActionNoDatabase() {
-
+    private function setupController(&$bookController, &$doctrineRegistry, &$entityManager) {
         $bookController = $this->getMockBuilder('Codebender\DefaultBundle\Controller\BookController')
             ->disableOriginalConstructor()
             ->setMethods(array('getDoctrine'))
@@ -23,7 +19,14 @@ class BookControllerUnitTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->setMethods(array('getRepository'))
             ->getMock();
-        
+    }
+    /**
+     * @expectedException \Exception
+     */
+    public function testIndexActionNoDatabase() {
+
+        $this->setupController($bookController, $doctrineRegistry, $entityManager);
+
         $bookRepository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
             ->getMock();
 
@@ -42,5 +45,33 @@ class BookControllerUnitTest extends \PHPUnit_Framework_TestCase
 
         $result = $bookController->indexAction();
         
+        $this->assertEquals(array('entities' => array()), $result);
+    }
+
+    public function testIndexActionNormalExecution() {
+
+        $this->setupController($bookController, $doctrineRegistry, $entityManager);
+        
+        $bookRepository = $this->getMockBuilder('Doctrine\ORM\EntityRepository')
+            ->disableOriginalConstructor()
+            ->setMethods(array('findAll'))
+            ->getMock();
+
+        $bookController->expects($this->once())->method('getDoctrine')
+            ->will($this->returnValue($doctrineRegistry));
+
+        $doctrineRegistry->expects($this->once())->method('getManager')
+            ->will($this->returnValue($entityManager));
+
+        $entityManager->expects($this->once())->method('getRepository')
+            ->with('CodebenderDefaultBundle:Book')
+            ->will($this->returnValue($bookRepository));
+
+        $bookRepository->expects($this->once())->method('findAll')
+            ->will($this->returnValue(array('entities & objects')));
+
+        $result = $bookController->indexAction();
+
+        $this->assertEquals(array('entities' => array('entities & objects')), $result);
     }
 }
